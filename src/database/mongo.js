4c9aa94db57af353c523160547b1e2dff1e29aeb
Tai Tran <107969594+MobaEnder@users.noreply.github.com>
2026-01-26 18:16:30 +0700

    Update mongo.js

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("✅ MongoDB Railway connected");
   } catch (err) {
     console.error("❌ MongoDB error:", err);
