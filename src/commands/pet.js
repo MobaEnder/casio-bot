ecce3e72da47d0011df69b71e1563959de582867
Tai Tran <107969594+MobaEnder@users.noreply.github.com>
2026-02-24 17:45:18 +0700

    fix

--- src/commands/pet.js
+++ src/commands/pet.js
@@ -64,25 +64,30 @@ module.exports = {
 if (interaction.customId.startsWith("pet_choose_"))
   return egg.handleChoose(interaction);
 
-    if (interaction.customId === "pet_menu_back") {
+ if (interaction.customId === "pet_menu_back") {
 
-      const row = new ActionRowBuilder().addComponents(
-        new ButtonBuilder()
-          .setCustomId("pet_shop")
-          .setLabel("🏪 Shop")
-          .setStyle(ButtonStyle.Primary),
+  const row = new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+      .setCustomId("pet_shop")
+      .setLabel("🏪 Shop")
+      .setStyle(ButtonStyle.Primary),
 
-        new ButtonBuilder()
-          .setCustomId("pet_balo")
-          .setLabel("🎒 Balo")
-          .setStyle(ButtonStyle.Secondary)
-      );
+    new ButtonBuilder()
+      .setCustomId("pet_balo")
+      .setLabel("🎒 Balo")
+      .setStyle(ButtonStyle.Secondary),
 
-      return interaction.update({
-        content: "🐲 **MENU THÚ CƯNG**",
-        components: [row]
-      });
-    }
+    new ButtonBuilder()
+      .setCustomId("pet_egg")
+      .setLabel("🥚 Nhận Pet")
+      .setStyle(ButtonStyle.Success)
+  );
+
+  return interaction.update({
+    content: "🐲 **MENU THÚ CƯNG**",
+    components: [row]
+  });
+}
 
   },
 
